5f06e6433ca2cb972e66e29c330d05ef9ad74637
dmprabath <dmprabath1995@gmail.com>
2020-06-22 22:33:48 +0530

    second one

---
+++ models/models.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class openacademy(models.Model):
+#     _name = 'openacademy.openacademy'
+#     _description = 'openacademy.openacademy'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
+from odoo import models, fields, api
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description ='Openacademy Course'
+
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text()
+    responsible_id = fields.Many2one('res.users', string='Responsible', ondelete='set null', index=True )
+    session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+    
+
+class Session(models.Model):
+    _name= 'openacademy.session'
+    _description = 'Openacadamy Sessions'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in Days")
+    seats = fields.Integer(string="Number of Seats")    
+    instructor_id = fields.Many2one('res.partner', string='Instructor')
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True )
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
